acbc309be55937722b95c635e7caee247942e4c0
yuanwang <yuan.wang@workiva.com>
2021-06-30 07:20:36 -0600

    update

--- spago.dhall
+++ spago.dhall
@@ -12,7 +12,7 @@ to generate this file without the comments in this block.
 -}
 { name = "my-project"
 , dependencies =
-  [ "arrays", "console", "effect", "prelude", "psci-support" ]
+  [ "arrays", "console", "effect", "newtype", "prelude", "psci-support" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,49 @@
 module Main where
 
 import Prelude
-import Data.Array (filter)
 
+import Data.Array (filter)
+import Data.Generic.Rep (class Generic)
+import Data.Newtype (class Newtype)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
 
 
+newtype CSV = CSV String
+derive newtype instance showCSV :: Show CSV
+
+class ToCSV a where
+  toCSV :: a -> CSV
+
+
+newtype FullName = FullName String
+derive newtype instance showFullName :: Show FullName
+
+newtype Age = Age Int
+derive instance newtypeAge :: Newtype Age _
+derive newtype instance showAge :: Show Age
+
+data Occupation = Doctor | Dentist | Lawyer | Unemployed
+derive instance genericOccupation :: Generic Occupation _
+
+instance showOccupation :: Show Occupation where
+  show = genericShow
+
+data Person = Person
+   { name :: FullName
+   , age :: Age
+   , occupation :: Occupation
+   }
+
+instance toCSVPerson :: ToCSV Person where
+  toCSV (Person {name, age, occupation}) = CSV $ show name <> "," <> show age <> "," <> show occupation
+
 even :: Int -> Boolean
-even i = mod i  2 == 0
+even i = i `mod`  2 == 0
 
 main :: Effect Unit
-main = do
-  (log <<< show <<< filter even) [1,2,3]
-  log "🍝"
+main = log $ show $ toCSV
+  (Person { name : FullName "Sue Smite"
+          , age : Age 23
+          , occupation: Doctor})
